81a709c2a059a8836feedcceb5cefb5b6658acd9
KirillRustyNail <63652169+KirillRustyNail@users.noreply.github.com>
2023-05-03 16:13:29 +0300

    Test new variant

---
+++ My_CSharp_AddIn/My_CSharp_AddIn/TestForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace My_CSharp_AddIn
+{
+    public partial class TestForm : Form
+    {
+        private Inventor.Application m_inventorAplication;
+
+        public TestForm(Inventor.Application application)
+        {
+            InitializeComponent();
+
+            m_inventorAplication = application;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Name = m_inventorAplication.ActiveDocument.DisplayName;
+
+            label1.Text = Name;
+
+
+
+        }
+    }
+}
